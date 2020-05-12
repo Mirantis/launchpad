b36fa31a72ecfe206a585823ab069b629c03a40c
Jussi Nummelin <jnummelin@users.noreply.github.com>
2020-05-12 13:18:38 +0300

    Move install command to cmd package (#2)
    Signed-off-by: Jussi Nummelin <jnummelin@mirantis.com>

---
+++ cmd/install.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"github.com/Mirantis/mcc/pkg/install"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
+)
+
+func NewInstallCommand() *cli.Command {
+	return &cli.Command{
+		Name:  "install",
+		Usage: "Install a new cluster",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "config",
+				Usage:   "Path to cluster config yaml",
+				Aliases: []string{"c"},
+				Value:   "cluster.yaml",
+			},
+		},
+		Action: func(ctx *cli.Context) error {
+			log.Info("Install called")
+
+			err := install.Install(ctx)
+			return err
+		},
+	}
+}
