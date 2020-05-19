2dfc3226b2e322e0ca4b2f0d95ea5fa9662cb0aa
Jussi Nummelin <jnummelin@mirantis.com>
2020-05-19 07:56:09 +0000

    make linter happy

--- pkg/phase/gather_facts.go
+++ pkg/phase/gather_facts.go
@@ -67,7 +67,7 @@ func (p *GatherFacts) Run(conf *config.ClusterConfig) error {
 // - if swarm is already initialized its cluster ID matches with the one in local state
 func (p *GatherFacts) validateFacts(config *config.ClusterConfig) error {
 	if config.Ucp.Metadata != nil && config.Ucp.Metadata.ClusterID != config.State.ClusterID {
-		return fmt.Errorf("cluster ID mismatch between local state (%s) and cluster state (%s). This configuration is probably for another cluster.", config.State.ClusterID, config.Ucp.Metadata.ClusterID)
+		return fmt.Errorf("cluster ID mismatch between local state (%s) and cluster state (%s). This configuration is probably for another cluster", config.State.ClusterID, config.Ucp.Metadata.ClusterID)
 	}
 
 	log.Infof("Facts check out against local state, safe to confinue")
