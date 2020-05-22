6b8e7ca33fcc0e0a408a4a68a9edad2cdac48fa8
Kimmo Lehto <kimmo.lehto@kontena.io>
2020-05-22 09:55:29 +0300

    Add sha256 sums for release binaries

--- release.sh
+++ release.sh
@@ -7,27 +7,35 @@ if [ -z "${TAG_NAME}" ]; then
   exit 1
 fi
 
+declare -a binaries=("mcc-darwin-x64" "mcc-win-x64.exe" "mcc-linux-x64")
+
+description="### Checksums\n\nFilename | Sha256\n---------|-------\n"
+
+for bin in "${binaries[@]}"
+do
+  filesum=$(sha256sum -b "./bin/${bin}" | cut -d" " -f1)
+  description=${description}"${bin} | ${filesum}\n"
+done
+
+echo -e "${description}"
+
 curl -L https://github.com/github-release/github-release/releases/download/v0.8.1/linux-amd64-github-release.bz2 > ./github-release.bz2
 bunzip2 ./github-release.bz2
 chmod +x ./github-release
 
-if echo "${TAG_NAME}" | grep "-" ; then
-  ./github-release release \
-    --pre-release \
-    --user Mirantis \
-    --repo mcc \
-    --tag "${TAG_NAME}" \
-    --name "${TAG_NAME}"
+if [[ "${TAG_NAME}" == *-* ]] ; then
+  releaseopt="--pre-release"
 else
-  ./github-release release \
-    --draft \
-    --user Mirantis \
-    --repo mcc \
-    --tag "${TAG_NAME}" \
-    --name "${TAG_NAME}"
+  releaseopt="--draft"
 fi
 
-declare -a binaries=("mcc-darwin-x64" "mcc-win-x64.exe" "mcc-linux-x64")
+echo -e "${description}" | ./github-release release \
+  $releaseopt \
+  --user Mirantis \
+  --repo mcc \
+  --tag "${TAG_NAME}" \
+  --name "${TAG_NAME}" \
+  --description -
 
 for bin in "${binaries[@]}"
 do
