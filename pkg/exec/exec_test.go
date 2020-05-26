36b8ae8ce0907f90ef333e01858f31bbaed19541
Kimmo Lehto <kimmo.lehto@kontena.io>
2020-05-26 16:02:07 +0300

    Some tests, need more

---
+++ pkg/exec/exec_test.go
@@ -0,0 +1,44 @@
+package exec
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestExec_Stdin(t *testing.T) {
+	opts := Options{}
+	Stdin("hello")(&opts)
+	require.Equal(t, "hello", opts.Stdin)
+}
+
+func TestExec_StreamOutput(t *testing.T) {
+	opts := Options{}
+	StreamOutput()(&opts)
+	require.True(t, opts.LogInfo)
+}
+
+func TestExec_HideCommand(t *testing.T) {
+	opts := Options{}
+	StreamOutput()(&opts)
+	require.False(t, opts.LogCommand)
+}
+
+func TestExec_HideOutput(t *testing.T) {
+	opts := Options{}
+	HideOutput()(&opts)
+	require.False(t, opts.LogDebug)
+}
+
+func TestExec_Redact(t *testing.T) {
+	opts := Options{}
+	Redact("hello")(&opts)
+	require.Equal(t, "hello", opts.Redact)
+}
+
+func TestExec_redactFunc(t *testing.T) {
+	f := redactFunc("hello")
+	require.Equal(t, "[REDACTED] world", f("hello world"))
+	f = redactFunc("(?i)hello")
+	require.Equal(t, "[REDACTED] world", f("Hello world"))
+}
