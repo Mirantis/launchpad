bdc3d43932f73f980c97599e47523685140bef6e
Kimmo Lehto <kimmo.lehto@gmail.com>
2020-05-27 13:43:40 +0300

    Update main.go
    Co-authored-by: Lauri Nevala <lnevala@mirantis.com>

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 
 	cli.AppHelpTemplate = fmt.Sprintf(`%s
 GETTING STARTED:
-    https://github.com/mirantis/launchpad/blob/master/docs/getting-started.md
+    https://github.com/Mirantis/launchpad/blob/master/docs/getting-started.md
 
 SUPPORT:
     https://github.com/mirantis/launchpad/issues
