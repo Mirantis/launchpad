129f1c6e3d63c10458c9283e626aa13fd302b1ac
Kimmo Lehto <kimmo.lehto@gmail.com>
2020-06-05 15:31:31 +0300

    The actual migrator

---
+++ pkg/apis/v1beta1/cluster.go
@@ -0,0 +1,51 @@
+package v1beta1
+
+import (
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v2"
+)
+
+// MigrateV1Beta2 migrates an v1beta1 format configuration into v1beta2 format and replaces the contents of the supplied data byte slice
+func MigrateV1Beta2(data *[]byte) error {
+	plain := make(map[string]interface{})
+	yaml.Unmarshal(*data, &plain)
+
+	plain["apiVersion"] = "launchpad.mirantis.com/v1beta2"
+
+	if plain["spec"] == nil {
+		return nil
+	}
+
+	hosts := plain["spec"].(map[interface{}]interface{})["hosts"]
+	hslice := hosts.([]interface{})
+
+	for _, h := range hslice {
+		host := h.(map[interface{}]interface{})
+		host["ssh"] = make(map[string]interface{})
+		ssh := host["ssh"].(map[string]interface{})
+		for k, v := range host {
+			switch k.(string) {
+			case "sshKeyPath":
+				ssh["keyPath"] = v
+				delete(host, k)
+				log.Debugf("migrated v1beta1 host sshKeyPath '%s' to v1beta2 ssh[keyPath]", v)
+			case "sshPort":
+				ssh["port"] = v
+				delete(host, k)
+				log.Debugf("migrated v1beta1 host sshPort '%d' to v1beta2 ssh[port]", v)
+			case "user":
+				ssh["user"] = v
+				delete(host, k)
+				log.Debugf("migrated v1beta1 host user '%s' to v1beta2 ssh[user]", v)
+			}
+		}
+	}
+	out, err := yaml.Marshal(&plain)
+	if err != nil {
+		return err
+	}
+
+	*data = out
+
+	return nil
+}
