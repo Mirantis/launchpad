afe76d946a303a0bd022b59ec70f70898f85905b
Kimmo Lehto <kimmo.lehto@gmail.com>
2020-06-05 12:28:17 +0300

    Use v1beta1 still in aws

--- examples/tf-aws/main.tf
+++ examples/tf-aws/main.tf
@@ -88,7 +88,7 @@ locals {
 
 output "ucp_cluster" {
   value = {
-    apiVersion = "launchpad.mirantis.com/v1beta2"
+    apiVersion = "launchpad.mirantis.com/v1beta1"
     kind = "UCP"
     spec = {
       ucp = {
