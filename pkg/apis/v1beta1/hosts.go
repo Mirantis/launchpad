7e1afd54a7c1afcc20464d6914f461ef0c59d0f6
Kimmo Lehto <kimmo.lehto@gmail.com>
2020-06-15 09:51:14 +0300

    Rename hosts.go to host.go
    Remove the Hosts from Host
    
    Add new hosts.go and hosts_test.go

--- pkg/apis/v1beta1/hosts.go
+++ pkg/apis/v1beta1/hosts.go
@@ -1,4 +1,81 @@
 package v1beta1
 
-// Hosts is the type alias for a slice of Hosts
+// Hosts is a collection of Hosts
 type Hosts []*Host
+
+// Filter returns a filtered list of Hosts. The filter function should returnn true for hosts matching the criteria.
+func (hosts *Hosts) Filter(filter func(host *Host) bool) Hosts {
+	result := make([]*Host, 0, len(*hosts))
+
+	for _, h := range *hosts {
+		if filter(h) {
+			result = append(result, h)
+		}
+	}
+
+	return result
+}
+
+// Find returns the first matching Host. The finder function should return true for a Host matching the criteria.
+func (hosts *Hosts) Find(filter func(host *Host) bool) *Host {
+	for _, h := range *hosts {
+		if filter(h) {
+			return (h)
+		}
+	}
+	return nil
+}
+
+// Index returns the index of the first matching Host. The finder function should return true for a Host matching the criteria.
+func (hosts *Hosts) Index(filter func(host *Host) bool) int {
+	for i, h := range *hosts {
+		if filter(h) {
+			return (i)
+		}
+	}
+	return -1
+}
+
+// IndexAll returns the indexes of the matching Hosts. The finder function should return true for a Host matching the criteria.
+func (hosts *Hosts) IndexAll(filter func(host *Host) bool) []int {
+	result := make([]int, 0, len(*hosts))
+	for i, h := range *hosts {
+		if filter(h) {
+			result = append(result, i)
+		}
+	}
+	return result
+}
+
+// Map returns a new slice which is the result of running the map function on each host.
+func (hosts *Hosts) Map(filter func(host *Host) interface{}) []interface{} {
+	result := make([]interface{}, len(*hosts))
+	for _, h := range *hosts {
+		result = append(result, filter(h))
+	}
+	return result
+}
+
+// MapString returns a new slice which is the result of running the map function on each host
+func (hosts *Hosts) MapString(filter func(host *Host) string) []string {
+	result := make([]string, len(*hosts))
+	for _, h := range *hosts {
+		result = append(result, filter(h))
+	}
+	return result
+}
+
+// Include returns true if any of the hosts match the filter function criteria.
+func (hosts *Hosts) Include(filter func(host *Host) bool) bool {
+	for _, h := range *hosts {
+		if filter(h) {
+			return true
+		}
+	}
+	return false
+}
+
+// Count returns the count of hosts matching the filter function criteria.
+func (hosts *Hosts) Count(filter func(host *Host) bool) int {
+	return len(hosts.IndexAll(filter))
+}
