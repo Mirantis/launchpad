86178b12528797362468fd6f4d65cc846fb57f4c
Kimmo Lehto <kimmo.lehto@gmail.com>
2020-06-17 13:36:11 +0300

    Install iptables on Centos

--- pkg/configurer/centos/centos.go
+++ pkg/configurer/centos/centos.go
@@ -11,6 +11,15 @@ type Configurer struct {
 	enterpriselinux.Configurer
 }
 
+// InstallBasePackages install all the needed base packages on the host
+func (c *Configurer) InstallBasePackages() error {
+	err := c.FixContainerizedHost()
+	if err != nil {
+		return err
+	}
+	return c.Host.Exec("sudo yum install -y curl socat iptables")
+}
+
 func resolveCentosConfigurer(h *api.Host) api.HostConfigurer {
 	if h.Metadata.Os.ID == "centos" {
 		return &Configurer{
