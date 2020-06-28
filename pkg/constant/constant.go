4c26d44863f4cf6d26fedc52a1c5bce364ac1497
Jari Kolehmainen <jkolehmainen@mirantis.com>
2020-06-28 17:16:31 +0000

    UCP v3.3.1
    Signed-off-by: Jari Kolehmainen <jkolehmainen@mirantis.com>

--- pkg/constant/constant.go
+++ pkg/constant/constant.go
@@ -4,7 +4,7 @@ const (
 	// ImageRepo is the default image repo to use
 	ImageRepo = "docker.io/docker"
 	// UCPVersion is the default UCP version to use
-	UCPVersion = "3.3.0"
+	UCPVersion = "3.3.1"
 	// EngineVersion is the default engine version
 	EngineVersion = "19.03.8"
 	// EngineChannel is the default engine channel
