8676770322f6c679aafcdfe75513c1c891a9ecdb
Kimmo Lehto <kimmo.lehto@gmail.com>
2020-08-12 10:19:32 +0300

    Add test for UseLegacyImageRepo

--- pkg/apis/v1beta3/ucp_config.go
+++ pkg/apis/v1beta3/ucp_config.go
@@ -2,6 +2,7 @@ package v1beta3
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/Mirantis/mcc/pkg/constant"
 	"github.com/Mirantis/mcc/pkg/util"
@@ -84,12 +85,19 @@ func NewUcpConfig() UcpConfig {
 
 func (c *UcpConfig) UseLegacyImageRepo(v *version.Version) bool {
 	// >=3.1.15 || >=3.2.8 || >=3.3.2 is "mirantis"
+	vs := v.String()
+	var v2 *version.Version
+	if strings.Contains(vs, "-") {
+		v2, _ = version.NewVersion(vs[0:strings.Index(vs, "-")])
+	} else {
+		v2 = v
+	}
 
 	c1, _ := version.NewConstraint("< 3.2, >= 3.1.15")
 	c2, _ := version.NewConstraint("> 3.1, < 3.3, >= 3.2.8")
 	c3, _ := version.NewConstraint("> 3.3, < 3.4, >= 3.3.2")
 	c4, _ := version.NewConstraint(">= 3.4")
-	return !(c1.Check(v) || c2.Check(v) || c3.Check(v) || c4.Check(v))
+	return !(c1.Check(v2) || c2.Check(v2) || c3.Check(v2) || c4.Check(v2))
 }
 
 // GetBootstrapperImage combines the bootstrapper image name based on user given config
--- pkg/apis/v1beta3/ucp_config_test.go
+++ pkg/apis/v1beta3/ucp_config_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/Mirantis/mcc/pkg/util"
+	"github.com/hashicorp/go-version"
 	"github.com/stretchr/testify/require"
 	"gopkg.in/yaml.v2"
 )
@@ -23,4 +24,33 @@ func TestUcpConfig_YAML_ConfigFile(t *testing.T) {
 	err := yaml.Unmarshal([]byte("configFile: test_path.toml"), &cfg)
 	require.NoError(t, err)
 	require.Equal(t, "abcd", cfg.ConfigData)
+}
+
+func TestUcpConfig_UseLegacyImageRepo(t *testing.T) {
+	cfg := UcpConfig{}
+	// >=3.1.15 || >=3.2.8 || >=3.3.2 is "mirantis"
+	legacyVersions := []string{
+		"3.1.14",
+		"3.2.7",
+		"3.3.1",
+		"2.0.0",
+		"3.2.7-tp7",
+	}
+	modernVersions := []string{
+		"3.1.15",
+		"3.1.16-rc1",
+		"3.2.8",
+		"3.3.2",
+		"4.0.0",
+	}
+
+	for _, vs := range legacyVersions {
+		v, _ := version.NewVersion(vs)
+		require.True(t, cfg.UseLegacyImageRepo(v), "should be true for %s", vs)
+	}
+
+	for _, vs := range modernVersions {
+		v, _ := version.NewVersion(vs)
+		require.False(t, cfg.UseLegacyImageRepo(v), "should be false for %s", vs)
+	}
 }
