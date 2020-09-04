3e207ef794175490eacd45f3429b70999686a8bc
James Nesbitt <jnesbitt@mirantis.com>
2020-09-04 12:46:16 -0400

    PRODENG-611 update DTR and ENGINE versions for smoke tests
    Signed-off-by: James Nesbitt <jnesbitt@mirantis.com>

--- test/smoke_upgrade.sh
+++ test/smoke_upgrade.sh
@@ -11,8 +11,8 @@ setup
 [ "${REUSE_CLUSTER}" = "" ] && ../bin/launchpad --debug apply
 
 export UCP_VERSION=${UCP_UPGRADE_VERSION:-"3.3.2"}
-export ENGINE_VERSION=${ENGINE_UPGRADE_VERSION:-"19.03.8"}
-export DTR_VERSION=${DTR_UPGRADE_VERSION:"2.8.1"}
+export ENGINE_VERSION=${ENGINE_UPGRADE_VERSION:-"19.03.12"}
+export DTR_VERSION=${DTR_UPGRADE_VERSION:"2.8.2"}
 
 generateYaml
 
