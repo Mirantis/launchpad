9893eaec7dbc7ddfa4bdb8b18bdde43b159a8061
Kimmo Lehto <kimmo.lehto@gmail.com>
2020-09-18 14:42:37 +0300

    Add shell auto-completions

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path"
 
 	"github.com/Mirantis/mcc/cmd"
 	"github.com/Mirantis/mcc/pkg/analytics"
+	"github.com/Mirantis/mcc/pkg/completion"
 	mcclog "github.com/Mirantis/mcc/pkg/log"
 	"github.com/Mirantis/mcc/version"
 	log "github.com/sirupsen/logrus"
@@ -28,6 +30,37 @@ func main() {
 		},
 	}
 
+	completionCmd := &cli.Command{
+		Name:   "completion",
+		Hidden: true,
+		Description: `Generates a shell auto-completion script.
+
+   Typical locations for the generated output are:
+   	- Bash: /etc/bash_completion.d/launchpad
+   	- Zsh: /usr/local/share/zsh/site-functions/_launchpad`,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "shell",
+				Usage:   "Shell to generate the script for",
+				Value:   "bash",
+				Aliases: []string{"s"},
+				EnvVars: []string{"SHELL"},
+			},
+		},
+		Action: func(ctx *cli.Context) error {
+			switch path.Base(ctx.String("shell")) {
+			case "bash":
+				fmt.Print(completion.BashTemplate())
+			case "zsh":
+				fmt.Print(completion.ZshTemplate())
+			default:
+				return fmt.Errorf("no completion script available for %s", ctx.String("shell"))
+			}
+
+			return nil
+		},
+	}
+
 	cli.AppHelpTemplate = fmt.Sprintf(`%s
 GETTING STARTED:
     https://github.com/Mirantis/launchpad/blob/master/docs/getting-started.md
@@ -37,8 +70,9 @@ SUPPORT:
 `, cli.AppHelpTemplate)
 
 	app := &cli.App{
-		Name:  "launchpad",
-		Usage: "Mirantis Launchpad",
+		Name:                 "launchpad",
+		Usage:                "Mirantis Launchpad",
+		EnableBashCompletion: true,
 		Flags: []cli.Flag{
 			&cli.BoolFlag{
 				Name:    "debug",
@@ -75,6 +109,7 @@ SUPPORT:
 			cmd.NewDownloadBundleCommand(),
 			cmd.NewResetCommand(),
 			cmd.NewInitCommand(),
+			completionCmd,
 			versionCmd,
 		},
 	}
---
+++ pkg/completion/completion.go
@@ -0,0 +1,65 @@
+package completion
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"strings"
+)
+
+func prog() string {
+	p, err := os.Executable()
+	if err != nil || strings.HasSuffix(p, "main") {
+		return "launchpad"
+	}
+	return path.Base(p)
+}
+
+func BashTemplate() string {
+	return fmt.Sprintf(`#! /bin/bash
+
+_launchpad_bash_autocomplete() {
+  if [[ "${COMP_WORDS[0]}" != "source" ]]; then
+    local cur opts base
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    if [[ "$cur" == "-"* ]]; then
+      opts=$( ${COMP_WORDS[@]:0:$COMP_CWORD} ${cur} --generate-bash-completion )
+    else
+      opts=$( ${COMP_WORDS[@]:0:$COMP_CWORD} --generate-bash-completion )
+    fi
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    return 0
+  fi
+}
+
+complete -o bashdefault -o default -o nospace -F _cli_bash_autocomplete %s
+`, prog())
+}
+
+func ZshTemplate() string {
+	p := prog()
+	return fmt.Sprintf(`#compdef %s
+
+_launchpad_zsh_autocomplete() {
+  local -a opts
+  local cur
+  cur=${words[-1]}
+  if [[ "$cur" == "-"* ]]; then
+    opts=("${(@f)$(_CLI_ZSH_AUTOCOMPLETE_HACK=1 ${words[@]:0:#words[@]-1} ${cur} --generate-bash-completion)}")
+  else
+    opts=("${(@f)$(_CLI_ZSH_AUTOCOMPLETE_HACK=1 ${words[@]:0:#words[@]-1} --generate-bash-completion)}")
+  fi
+
+  if [[ "${opts[1]}" != "" ]]; then
+    _describe 'values' opts
+  else
+    _files
+  fi
+
+  return
+}
+
+compdef _launchpad_zsh_autocomplete %s
+`, p, p)
+}
