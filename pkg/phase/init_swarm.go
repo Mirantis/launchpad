7060e30f9567a8ab33c222732b452bb638fa1907
Kimmo Lehto <kimmo.lehto@gmail.com>
2020-09-29 09:55:28 +0300

    Even more hiding

--- pkg/phase/init_swarm.go
+++ pkg/phase/init_swarm.go
@@ -35,7 +35,7 @@ func (p *InitSwarm) Run() error {
 		log.Infof("%s: swarm already initialized", swarmLeader.Address)
 	}
 
-	mgrToken, err := swarmLeader.ExecWithOutput(swarmLeader.Configurer.DockerCommandf("swarm join-token manager -q"))
+	mgrToken, err := swarmLeader.ExecWithOutput(swarmLeader.Configurer.DockerCommandf("swarm join-token manager -q"), exec.HideOutput())
 	if err != nil {
 		return NewError("failed to get swarm manager join-token")
 	}
