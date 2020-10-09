de87dbfc90334a5b536db2084092cbda6fdaee00
Kimmo Lehto <kimmo.lehto@gmail.com>
2020-10-09 14:46:12 +0300

    Chain migrations (#211)
    * Chain migrations
    
    * Fix tests, reunmarshal

--- pkg/api/v1beta3/cluster_test.go
+++ pkg/api/v1beta3/cluster_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestMigrationToCurrent(t *testing.T) {
+func TestMigration(t *testing.T) {
 	b3 := []byte(`---
 apiVersion: "launchpad.mirantis.com/v1beta3"
 kind: DockerEnterprise
@@ -23,35 +23,6 @@ spec:
   - address: 10.0.0.1
     role: manager
 `)
-	require.NoError(t, MigrateToCurrent(&b3))
+	require.NoError(t, Migrate(&b3))
 	require.Equal(t, b4, b3)
-}
-
-func TestMigrationToCurrentWithHooks(t *testing.T) {
-	b3 := []byte(`---
-apiVersion: "launchpad.mirantis.com/v1beta3"
-kind: DockerEnterprise
-spec:
-  hosts:
-    - address: "10.0.0.1"
-      role: "manager"
-      hooks:
-        apply:
-          before:
-            - ls -al
-`)
-	require.EqualError(t, MigrateToCurrent(&b3), "host hooks require apiVersion >= launchpad.mirantis.com/v1")
-}
-
-func TestMigrationToCurrentWithLocalhost(t *testing.T) {
-	b3 := []byte(`---
-apiVersion: "launchpad.mirantis.com/v1beta3"
-kind: DockerEnterprise
-spec:
-  hosts:
-    - address: "10.0.0.1"
-      role: "manager"
-      localhost: true
-`)
-	require.EqualError(t, MigrateToCurrent(&b3), "localhost connection requires apiVersion >= launchpad.mirantis.com/v1")
 }
