3e354e662ab8cc7d56e68bec622fd89c352b4f98
Kimmo Lehto <kimmo.lehto@gmail.com>
2020-10-12 16:04:40 +0300

    Need to investigate

--- test/smoke_apply_local.sh
+++ test/smoke_apply_local.sh
@@ -10,11 +10,11 @@ trap cleanup EXIT
 
 echo SMOKE_DIR=$SMOKE_DIR
 
-downloadFootloose
-generateKey
-createCluster
+setup
+
 ./footloose status worker0 -o json
 WORKER_IP=$(./footloose status worker0 -o json | grep "\"ip\": \"172" | head -1 |cut -d\" -f4)
+echo WORKER_IP: $WORKER_IP
 
 ./footloose ssh root@manager0 "cd /launchpad/test; WORKER_IP=${WORKER_IP} CLUSTER_NAME=${CLUSTER_NAME} UCP_VERSION=${UCP_VERSION} UCP_IMAGE_REPO=${UCP_IMAGE_REPO} ENGINE_VERSION=${ENGINE_VERSION} DISABLE_TELEMETRY=true ACCEPT_LICENSE=true ../bin/launchpad --debug apply --config ${LAUNCHPAD_CONFIG}"
 ./footloose ssh root@manager0 "cd /launchpad/test; WORKER_IP=${WORKER_IP} CLUSTER_NAME=${CLUSTER_NAME} UCP_VERSION=${UCP_VERSION} UCP_IMAGE_REPO=${UCP_IMAGE_REPO} ENGINE_VERSION=${ENGINE_VERSION} DISABLE_TELEMETRY=true ACCEPT_LICENSE=true ../bin/launchpad --debug describe --config ${LAUNCHPAD_CONFIG} hosts"
