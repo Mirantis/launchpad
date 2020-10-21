74878e451a1a07d90e7604f28fdcdc411a0ef5c1
Kimmo Lehto <kimmo.lehto@gmail.com>
2020-10-21 14:27:54 +0300

    Fix pulling DTR images from custom repositories (#226)
    * Fix pulling DTR images from custom repositories
    
    * Add a test

--- pkg/phase/pull_images_test.go
+++ pkg/phase/pull_images_test.go
@@ -9,4 +9,5 @@ import (
 func TestPullImagesCustomRepo(t *testing.T) {
 	phase := PullImages{}
 	require.Equal(t, "xyz/foofoo:1.2.3-latest", phase.ImageFromCustomRepo("docker/foofoo:1.2.3-latest", "xyz"))
+	require.Equal(t, "xyz/foofoo:1.2.3-latest", phase.ImageFromCustomRepo("foo.example.com/docker/foofoo:1.2.3-latest", "xyz"))
 }
