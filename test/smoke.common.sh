ce976373f4f20c717cccdfb3a09c840c3129a224
Kimmo Lehto <kimmo.lehto@gmail.com>
2020-10-29 10:47:16 +0200

    Better cleanup, unify

--- test/smoke.common.sh
+++ test/smoke.common.sh
@@ -98,3 +98,52 @@ function setup() {
   fi
   generateYaml
 }
+
+function cloneImages() {
+  echo "Setting up a temporary footloose machine for pushing images"
+  envsubst < footloose-localrepo.yaml.tpl > footloose-localrepo.yaml
+  FLOPT="--config footloose-localrepo.yaml"
+  ./footloose delete $FLOPT || true
+  ./footloose create $FLOPT
+  ./footloose ssh $FLOPT root@pusher0 -- \
+    'apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y apt-utils && \
+     curl -fsSL https://get.docker.com | DEBIAN_FRONTEND=noninteractive bash -s && \
+     mkdir -p /etc/docker && \
+     echo "{ \"insecure-registries\":[\"172.16.86.100:5000\"] }" > /etc/docker/daemon.json && \
+     service docker restart'
+
+  docker rm --force /registry || true
+  docker run --name registry -d --network footloose-cluster --expose 5000 --ip 172.16.86.100 registry:latest
+
+  echo "Pulling + pushing UCP images..."
+
+  docker pull $UCP_IMAGE_REPO/ucp:$UCP_VERSION
+  for image in $(docker run --rm ${UCP_IMAGE_REPO}/ucp:${UCP_VERSION} images --list | sort | uniq); do
+    imagebase=$(basename ${image})
+    imagebase=${imagebase%:*}
+    fullimage="$UCP_IMAGE_REPO/$imagebase:$UCP_VERSION"
+    ./footloose ssh $FLOPT root@pusher0 -- \
+      "docker pull ${fullimage} && \
+       docker tag ${fullimage} 172.16.86.100:5000/test/$imagebase:$UCP_VERSION && \
+       docker push 172.16.86.100:5000/test/$imagebase:$UCP_VERSION"
+  done
+
+  echo "Pulling + pushing DTR images..."
+
+  docker pull $DTR_IMAGE_REPO/dtr:$DTR_VERSION
+  for image in $(docker run --rm ${DTR_IMAGE_REPO}/dtr:${DTR_VERSION} images | sort | uniq); do
+    imagebase=$(basename ${image})
+    imagebase=${imagebase%:*}
+    fullimage="$DTR_IMAGE_REPO/$imagebase:$DTR_VERSION"
+    ./footloose ssh $FLOPT root@pusher0 -- \
+      "docker pull ${fullimage} && \
+       docker tag ${fullimage} 172.16.86.100:5000/test/$imagebase:$DTR_VERSION && \
+       docker push 172.16.86.100:5000/test/$imagebase:$DTR_VERSION"
+  done
+
+  ./footloose delete $FLOPT
+
+  export UCP_IMAGE_REPO=172.16.86.100:5000/test
+  export DTR_IMAGE_REPO=172.16.86.100:5000/test
+}
+
