88e33ee0fbb2a37e8b066fdf0f8332a6dbc927f1
Kimmo Lehto <kimmo.lehto@gmail.com>
2020-12-03 09:58:45 +0200

    It may actually work like this

--- pkg/phase/manager.go
+++ pkg/phase/manager.go
@@ -63,7 +63,7 @@ func (m *Manager) Run() error {
 
 		text := aurora.Green("==> Running phase: %s").String()
 		log.Infof(text, phase.Title())
-		if p, ok := interface{}(phase).(Eventable); ok {
+		if p, ok := interface{}(phase).(interface{ Eventable }); ok {
 			start := time.Now()
 			r := reflect.ValueOf(m.config).Elem()
 			props := event.Properties{
