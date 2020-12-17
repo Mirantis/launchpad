e2da8ad58115be5627be2a355b8278b108b20e9c
Kimmo Lehto <kimmo.lehto@gmail.com>
2020-12-17 12:49:21 +0200

    Preliminary SLES support (#278)

---
+++ pkg/configurer/sles/sles.go
@@ -0,0 +1,55 @@
+package sles
+
+import (
+	"github.com/Mirantis/mcc/pkg/configurer"
+	"github.com/Mirantis/mcc/pkg/configurer/resolver"
+	common "github.com/Mirantis/mcc/pkg/product/common/api"
+)
+
+// Configurer is a generic Ubuntu level configurer implementation. Some of the configurer interface implementation
+// might be on OS version specific implementation such as for Bionic.
+type Configurer struct {
+	configurer.LinuxConfigurer
+}
+
+// InstallMKEBasePackages installs the needed base packages on Ubuntu
+func (c *Configurer) InstallMKEBasePackages() error {
+	err := c.FixContainerizedHost()
+	if err != nil {
+		return err
+	}
+	return c.Host.Exec("sudo zypper -n install -y curl socat")
+}
+
+// UninstallEngine uninstalls docker-ee engine
+func (c *Configurer) UninstallEngine(scriptPath string, engineConfig common.EngineConfig) error {
+	err := c.Host.Exec("sudo docker system prune -f")
+	if err != nil {
+		return err
+	}
+	err = c.Host.Exec("sudo systemctl stop docker")
+	if err != nil {
+		return err
+	}
+	err = c.Host.Exec("sudo systemctl stop containerd")
+	if err != nil {
+		return err
+	}
+	return c.Host.Exec("sudo zypper -n remove -y --clean-deps docker-ee docker-ee-cli")
+}
+
+func resolveSLESConfigurer(h configurer.Host, os *common.OsRelease) interface{} {
+	if os.ID == "sles" {
+		return &Configurer{
+			LinuxConfigurer: configurer.LinuxConfigurer{
+				Host: h,
+			},
+		}
+	}
+
+	return nil
+}
+
+func init() {
+	resolver.RegisterHostConfigurer(resolveSLESConfigurer)
+}
