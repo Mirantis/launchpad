e5bc307bda095f27c8d1fe47a7bf3884a5a3f389
Kimmo Lehto <kimmo.lehto@gmail.com>
2020-12-23 11:05:12 +0200

    Upgrade default versions: MKE3.3.5, MSR:2.8.5, Engine: 19.03.14 (#282)

--- pkg/constant/constant.go
+++ pkg/constant/constant.go
@@ -6,9 +6,9 @@ const (
 	// ImageRepoLegacy is the default image repo to use for older versions
 	ImageRepoLegacy = "docker.io/docker"
 	// MKEVersion is the default MKE version to use
-	MKEVersion = "3.3.4"
+	MKEVersion = "3.3.5"
 	// MSRVersion is the default MSR version to use
-	MSRVersion = "2.8.4"
+	MSRVersion = "2.8.5"
 	// MCRVersion is the default engine version
 	MCRVersion = "19.03.14"
 	// MCRChannel is the default engine channel
