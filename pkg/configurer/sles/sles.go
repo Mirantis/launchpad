b1fdb9d7dd3209e750469cecdb2855d831d6db23
Kimmo Lehto <kimmo.lehto@gmail.com>
2021-04-12 09:32:24 +0300

    Use "ip addr show" instead of "hostname --all-ip-addresses" on SLES (#325)
    * Install "hostname" package on SLES
    
    * Use "ip addr show"
    
    * No need for the "hostname" package

--- pkg/configurer/sles/sles.go
+++ pkg/configurer/sles/sles.go
@@ -1,6 +1,8 @@
 package sles
 
 import (
+	"strings"
+
 	"github.com/Mirantis/mcc/pkg/configurer"
 	common "github.com/Mirantis/mcc/pkg/product/common/api"
 	"github.com/k0sproject/rig"
@@ -37,6 +39,16 @@ func (c Configurer) UninstallMCR(h os.Host, scriptPath string, engineConfig comm
 	}
 
 	return h.Exec("sudo zypper -n remove -y --clean-deps docker-ee docker-ee-cli")
+}
+
+// LocalAddresses returns a list of local addresses, SLES12 has an old version of "hostname" without "--all-ip-addresses" and because of that, ip addr show is used here.
+func (c Configurer) LocalAddresses(h os.Host) ([]string, error) {
+	output, err := h.ExecOutput("ip addr show | grep 'inet ' | awk '{print $2}' | cut -d/ -f1")
+	if err != nil {
+		return nil, err
+	}
+
+	return strings.Fields(output), nil
 }
 
 func init() {
