fb0857daa092f353bf5f6b31ca6cdfa2c771b4eb
Kimmo Lehto <kimmo.lehto@gmail.com>
2021-04-14 15:29:17 +0300

    Figure bastion port programmatically

--- test/smoke_apply_bastion.sh
+++ test/smoke_apply_bastion.sh
@@ -9,11 +9,13 @@ trap cleanup EXIT
 setup && downloadTools
 
 BASTION_IP=$(./footloose status bastion0 -o json | grep "\"ip\": \"172" | head -1 |cut -d\" -f4)
+BASTION_PORT=$(docker inspect mke-bastion0|grep -A3 22/tcp|grep HostPort|head -1|cut -d\" -f4)
 MANAGER_IP=$(./footloose status manager0 -o json | grep "\"ip\": \"172" | head -1 |cut -d\" -f4)
 WORKER_IP=$(./footloose status worker0 -o json | grep "\"ip\": \"172" | head -1 |cut -d\" -f4)
 export MANAGER_IP
 export WORKER_IP
 export BASTION_IP
+export BASTION_PORT
 
 ${LAUNCHPAD} apply --debug --config ${LAUNCHPAD_CONFIG}
 
