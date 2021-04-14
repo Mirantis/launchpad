c72656a6bb0e0e4e42798d1e8f87e1bc73a8c85c
Kimmo Lehto <kimmo.lehto@gmail.com>
2021-04-14 16:31:06 +0300

    Silly typo

--- test/smoke_apply_forward.sh
+++ test/smoke_apply_forward.sh
@@ -19,5 +19,5 @@ BASTION_PORT=$(docker inspect mke-bastion0|grep -A3 22/tcp|grep HostPort|head -1
 
 eval $(ssh-agent -s)
 ssh-add ./id_rsa_launchpad
-ssh -o StrictHostKeyChecking=no -A -i ./id_rsa_launchpad -p ${BASTION_PORT} 2root@localhost "cd /launchpad/test; MANAGER_IP=${MANAGER_IP} WORKER_IP=${WORKER_IP} CLUSTER_NAME=${CLUSTER_NAME} MKE_VERSION=${MKE_VERSION} MKE_IMAGE_REPO=${MKE_IMAGE_REPO} MCR_VERSION=${MCR_VERSION} DISABLE_TELEMETRY=true ACCEPT_LICENSE=true ../bin/launchpad apply --debug --config ${LAUNCHPAD_CONFIG}"
+ssh -o StrictHostKeyChecking=no -A -i ./id_rsa_launchpad -p ${BASTION_PORT} root@localhost "cd /launchpad/test; MANAGER_IP=${MANAGER_IP} WORKER_IP=${WORKER_IP} CLUSTER_NAME=${CLUSTER_NAME} MKE_VERSION=${MKE_VERSION} MKE_IMAGE_REPO=${MKE_IMAGE_REPO} MCR_VERSION=${MCR_VERSION} DISABLE_TELEMETRY=true ACCEPT_LICENSE=true ../bin/launchpad apply --debug --config ${LAUNCHPAD_CONFIG}"
 ssh -o StrictHostKeyChecking=no -A -i ./id_rsa_launchpad -p ${BASTION_PORT} root@localhost "cd /launchpad/test; MANAGER_IP=${MANAGER_IP} WORKER_IP=${WORKER_IP} CLUSTER_NAME=${CLUSTER_NAME} MKE_VERSION=${MKE_VERSION} MKE_IMAGE_REPO=${MKE_IMAGE_REPO} MCR_VERSION=${MCR_VERSION} DISABLE_TELEMETRY=true ACCEPT_LICENSE=true ../bin/launchpad exec --debug --config ${LAUNCHPAD_CONFIG} --all --parallel hostname"
