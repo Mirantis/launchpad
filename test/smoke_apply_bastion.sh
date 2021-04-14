5b7f9cfcccfe075bf2579a4d91dae63ecdecf6c2
Kimmo Lehto <kimmo.lehto@gmail.com>
2021-04-14 14:21:43 +0300

    Need exports?

--- test/smoke_apply_bastion.sh
+++ test/smoke_apply_bastion.sh
@@ -11,6 +11,9 @@ setup && downloadTools
 BASTION_IP=$(./footloose status bastion0 -o json | grep "\"ip\": \"172" | head -1 |cut -d\" -f4)
 MANAGER_IP=$(./footloose status manager0 -o json | grep "\"ip\": \"172" | head -1 |cut -d\" -f4)
 WORKER_IP=$(./footloose status worker0 -o json | grep "\"ip\": \"172" | head -1 |cut -d\" -f4)
+export MANAGER_IP
+export WORKER_IP
+export BASTION_IP
 
 ${LAUNCHPAD} apply --debug --config ${LAUNCHPAD_CONFIG}
 
--- test/smoke_apply_forward.sh
+++ test/smoke_apply_forward.sh
@@ -12,6 +12,8 @@ setup && downloadTools
 
 MANAGER_IP=$(./footloose status manager0 -o json | grep "\"ip\": \"172" | head -1 |cut -d\" -f4)
 WORKER_IP=$(./footloose status worker0 -o json | grep "\"ip\": \"172" | head -1 |cut -d\" -f4)
+export MANAGER_IP
+export WORKER_IP
 
 eval $(ssh-agent -s)
 ssh-add ./id_rsa_launchpad
