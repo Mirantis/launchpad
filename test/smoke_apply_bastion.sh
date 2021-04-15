007a7922f3072f3a714e32a4925d9e659870f678
Kimmo Lehto <kimmo.lehto@gmail.com>
2021-04-15 15:45:17 +0300

    It should work??

--- test/smoke_apply_bastion.sh
+++ test/smoke_apply_bastion.sh
@@ -10,12 +10,13 @@ trap cleanup EXIT
 
 setup && downloadTools
 
-BASTION_PORT=$(./footloose status bastion0 -o json|grep -B1 'containerPort": 22'|grep hostPort|awk '{print $2}'|sed 's/,//')
+BASTION_PORT=$(docker inspect mke-bastion0|grep -A3 22/tcp|grep HostPort|head -1|cut -d\" -f4)
 MANAGER_IP=$(./footloose status manager0 -o json | grep "\"ip\": \"172" | head -1 |cut -d\" -f4)
 WORKER_IP=$(./footloose status worker0 -o json | grep "\"ip\": \"172" | head -1 |cut -d\" -f4)
 export MANAGER_IP
 export WORKER_IP
 export BASTION_PORT
+echo "bastion port: $BASTION_PORT"
 
 ${LAUNCHPAD} apply --debug --config ${LAUNCHPAD_CONFIG}
 
