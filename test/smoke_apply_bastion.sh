9ccac0315c68070784ee4362754cd685fcc40667
Kimmo Lehto <kimmo.lehto@gmail.com>
2021-04-15 13:57:27 +0300

    Bastion port empty?

--- test/smoke_apply_bastion.sh
+++ test/smoke_apply_bastion.sh
@@ -8,7 +8,7 @@ trap cleanup EXIT
 
 setup && downloadTools
 
-BASTION_PORT=$(docker inspect mke-bastion0|grep -A3 22/tcp|grep HostPort|head -1|cut -d\" -f4)
+BASTION_PORT=$(./footloose status bastion0 -o json|grep -B1 'containerPort": 22'|grep hostPort|awk '{print $2}'|sed 's/,//')
 MANAGER_IP=$(./footloose status manager0 -o json | grep "\"ip\": \"172" | head -1 |cut -d\" -f4)
 WORKER_IP=$(./footloose status worker0 -o json | grep "\"ip\": \"172" | head -1 |cut -d\" -f4)
 export MANAGER_IP
