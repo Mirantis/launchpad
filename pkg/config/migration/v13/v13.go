c2987e496c7d1f2db6726a7aee6c42085b7b09cc
Kimmo Lehto <kimmo.lehto@gmail.com>
2021-04-28 10:39:00 +0300

    Config migration sets defaults when missing/blank

--- pkg/config/migration/v13/v13.go
+++ pkg/config/migration/v13/v13.go
@@ -8,6 +8,46 @@ import (
 // Migrate migrates an v1.2 format configuration into the v1.3 api format and replaces the contents of the supplied data byte slice
 func Migrate(plain map[string]interface{}) error {
 	plain["apiVersion"] = "launchpad.mirantis.com/mke/v1.4"
+	if spec, ok := plain["spec"].(map[interface{}]interface{}); ok {
+		if mke, ok := spec["mke"].(map[interface{}]interface{}); ok {
+			if mkev, ok := mke["version"].(string); ok {
+				if mkev == "" {
+					mke["version"] = "3.4.0"
+					log.Debugf("migration defaulted MKE version to %s as an explicit version is required on the v1.4 api", mke["version"])
+				}
+			}
+		} else {
+			spec["mke"] = map[string]string{"version": "3.4.0"}
+			log.Debugf("migration defaulted MKE version to %s as an explicit version is required on the v1.4 api", mke["version"])
+		}
+
+		if msr, ok := spec["msr"].(map[interface{}]interface{}); ok {
+			if msrv, ok := msr["version"].(string); !ok || msrv == "" {
+				msr["version"] = "2.9.0"
+				log.Debugf("migration defaulted msr version to %s as an explicit version is required on the v1.4 api", msr["version"])
+			}
+		} else {
+			kind, ok := plain["kind"].(string)
+			if ok && kind != "mke+msr" {
+				if hosts, ok := spec["hosts"]; ok {
+					hslice := hosts.([]interface{})
+					for _, h := range hslice {
+						if host, ok := h.(map[interface{}]interface{}); ok {
+							if role, ok := host["role"].(string); ok && role == "msr" {
+								kind = "mke+msr"
+								break
+							}
+						}
+					}
+				}
+
+				if kind == "mke+msr" {
+					spec["msr"] = &(map[string]string{"version": "2.9.0"})
+					log.Debugf("migration defaulted msr version to %s as an explicit version is required on the v1.4 api", msr["version"])
+				}
+			}
+		}
+	}
 
 	log.Debugf("migrated configuration from launchpad.mirantis.com/v1.2 to launchpad.mirantis.com/mke/v1.3")
 	log.Infof("Note: The configuration has been migrated from a previous version")
--- pkg/config/migration/v13/v13_test.go
+++ pkg/config/migration/v13/v13_test.go
@@ -7,38 +7,91 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-func TestApiVersionMigration(t *testing.T) {
+func TestVersionMigration(t *testing.T) {
 	v13 := []byte(`---
 apiVersion: launchpad.mirantis.com/mke/v1.3
 kind: mke
 spec:
   hosts:
-  - ssh:
+  - role: manager
+    ssh:
       address: 10.0.0.1
       port: 22
-  - ssh:
+  - role: worker
+    ssh:
       address: 10.0.0.2
-  - winRM:
+  - role: msr
+    ssh:
       address: 10.0.0.3
-      port: 2000
-  - localhost:
-      enabled: true
 `)
 	// go's YAML marshal does not add the --- header
 	v14 := []byte(`apiVersion: launchpad.mirantis.com/mke/v1.4
 kind: mke
 spec:
   hosts:
-  - ssh:
+  - role: manager
+    ssh:
       address: 10.0.0.1
       port: 22
-  - ssh:
+  - role: worker
+    ssh:
       address: 10.0.0.2
-  - winRM:
+  - role: msr
+    ssh:
       address: 10.0.0.3
-      port: 2000
-  - localhost:
-      enabled: true
+  mke:
+    version: 3.4.0
+  msr:
+    version: 2.9.0
+`)
+	in := make(map[string]interface{})
+	require.NoError(t, yaml.Unmarshal(v13, in))
+	require.NoError(t, Migrate(in))
+	out, err := yaml.Marshal(in)
+	require.NoError(t, err)
+	require.Equal(t, string(v14), string(out))
+}
+
+func TestVersionMigrationNoOverwrite(t *testing.T) {
+	v13 := []byte(`---
+apiVersion: launchpad.mirantis.com/mke/v1.3
+kind: mke
+spec:
+  hosts:
+  - role: manager
+    ssh:
+      address: 10.0.0.1
+      port: 22
+  - role: worker
+    ssh:
+      address: 10.0.0.2
+  - role: msr
+    ssh:
+      address: 10.0.0.3
+  mke:
+    version: 3.3.7
+  msr:
+    version: 2.8.5
+`)
+	// go's YAML marshal does not add the --- header
+	v14 := []byte(`apiVersion: launchpad.mirantis.com/mke/v1.4
+kind: mke
+spec:
+  hosts:
+  - role: manager
+    ssh:
+      address: 10.0.0.1
+      port: 22
+  - role: worker
+    ssh:
+      address: 10.0.0.2
+  - role: msr
+    ssh:
+      address: 10.0.0.3
+  mke:
+    version: 3.3.7
+  msr:
+    version: 2.8.5
 `)
 	in := make(map[string]interface{})
 	require.NoError(t, yaml.Unmarshal(v13, in))
