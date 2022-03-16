213d6a7c7ee5a8e8d2967d37248c97ab3c5eb35a
Kimmo Lehto <kimmo.lehto@gmail.com>
2022-03-16 13:48:35 +0200

    Add support for Rocky Linux (#360)
    * Add support for Rocky Linux
    
    * Lint

---
+++ pkg/configurer/enterpriselinux/rockylinux.go
@@ -0,0 +1,22 @@
+package enterpriselinux
+
+import (
+	"github.com/k0sproject/rig"
+	"github.com/k0sproject/rig/os/registry"
+)
+
+// RockyLinux support
+type RockyLinux struct {
+	Configurer
+}
+
+func init() {
+	registry.RegisterOSModule(
+		func(os rig.OSVersion) bool {
+			return os.ID == "rocky"
+		},
+		func() interface{} {
+			return RockyLinux{}
+		},
+	)
+}
