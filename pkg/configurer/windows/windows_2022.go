b3b8acff33008223c8e155a543fb8c1b9d8183ca
Jim Carroll <pcarroll@mirantis.com>
2022-04-07 02:19:16 -0500

    Add Windows Server 2022 support (#362)

---
+++ pkg/configurer/windows/windows_2022.go
@@ -0,0 +1,29 @@
+package windows
+
+import (
+	"github.com/Mirantis/mcc/pkg/configurer"
+	"github.com/k0sproject/rig"
+	"github.com/k0sproject/rig/os"
+	"github.com/k0sproject/rig/os/registry"
+)
+
+// Windows2022Configurer is a Windows 2019 configurer implementation.
+type Windows2022Configurer struct {
+	configurer.WindowsConfigurer
+}
+
+// InstallMKEBasePackages installs the needed base packages on Ubuntu
+func (c Windows2022Configurer) InstallMKEBasePackages(_ os.Host) error {
+	return nil
+}
+
+func init() {
+	registry.RegisterOSModule(
+		func(os rig.OSVersion) bool {
+			return os.ID == "windows" && os.Version == "10.0.20348"
+		},
+		func() interface{} {
+			return Windows2022Configurer{}
+		},
+	)
+}
