aa68d88ce33667fe7291fbc0d0812ed9744612eb
Jim Carroll <pcarroll@mirantis.com>
2022-05-11 02:50:46 -0500

    Add support for Ubuntu 22.04 (#367)

---
+++ pkg/configurer/ubuntu/jammy.go
@@ -0,0 +1,22 @@
+package ubuntu
+
+import (
+	"github.com/k0sproject/rig"
+	"github.com/k0sproject/rig/os/registry"
+)
+
+// JammyConfigurer is the Ubuntu Focal (20.04) specific host configurer implementation
+type JammyConfigurer struct {
+	Configurer
+}
+
+func init() {
+	registry.RegisterOSModule(
+		func(os rig.OSVersion) bool {
+			return os.ID == "ubuntu" && os.Version == "22.04"
+		},
+		func() interface{} {
+			return JammyConfigurer{}
+		},
+	)
+}
