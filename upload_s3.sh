25c253f867ca2059b1ee51adb7551ec9947e1db4
Dimitar Dimitrov <ddimitrov@mirantis.com>
2024-04-04 15:04:39 +0300

    PRODENG-2518 Fixed uploading of artifacts to s3
    Signed-off-by: Dimitar Dimitrov <ddimitrov@mirantis.com>

--- upload_s3.sh
+++ upload_s3.sh
@@ -3,5 +3,5 @@
 TAG=$(git describe --tags --abbrev=0)
 
 for file in dist/release/*; do
-    aws s3 cp "$file" s3://get-mirantis.com/launchpad/$TAG/"$(basename "$file")" --recursive
+    aws s3 cp "$file" s3://get-mirantis.com/launchpad/$TAG/"$(basename "$file")"
 done
