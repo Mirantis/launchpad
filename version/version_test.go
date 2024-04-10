fbeaa2c7bed8e798035c89e8767964d139f3e046
Dimitar Dimitrov <ddimitrov@mirantis.com>
2024-04-10 09:09:40 +0300

    Unit tests for the version.go file
    Signed-off-by: Dimitar Dimitrov <ddimitrov@mirantis.com>

---
+++ version/version_test.go
@@ -0,0 +1,74 @@
+package version_test
+
+import (
+	"fmt"
+	"runtime"
+	"testing"
+
+	"github.com/Mirantis/mcc/version"
+)
+
+func TestIsDevelopment(t *testing.T) {
+	version.Environment = "development"
+	if version.IsProduction() {
+		t.Error("Expected development, got production")
+	}
+}
+
+func TestIsProduction(t *testing.T) {
+	version.Environment = "production"
+	if !version.IsProduction() {
+		t.Error("Expected production, got development")
+	}
+}
+
+func TestAssetIsForHost(t *testing.T) {
+	os := runtime.GOOS
+	arch := runtime.GOARCH
+	asset := version.Asset{Name: fmt.Sprintf("launchpad_%s_%s_0.0.0", os, arch)}
+
+	if !asset.IsForHost() {
+		t.Error("Expected asset to be for host")
+	}
+}
+
+func TestAssetIsNotForHost(t *testing.T) {
+	asset := version.Asset{Name: "launchpad_linux_badarchx64_0.0.0"}
+
+	if asset.IsForHost() {
+		t.Error("Expected asset to not be for host")
+	}
+}
+
+func TestLaunchpadReleaseIsNewer(t *testing.T) {
+	remote := version.LaunchpadRelease{TagName: "0.0.1"}
+
+	if !remote.IsNewer() {
+		t.Error("Expected remote to be newer")
+	}
+}
+
+func TestLaunchpadReleaseIsEqual(t *testing.T) {
+	remote := version.LaunchpadRelease{TagName: "0.0.0"}
+
+	if remote.IsNewer() {
+		t.Error("Expected remote to not be newer")
+	}
+}
+
+func TestLaunchpadReleaseIsOlder(t *testing.T) {
+	version.Version = "0.0.1"
+	remote := version.LaunchpadRelease{TagName: "0.0.0"}
+
+	if remote.IsNewer() {
+		t.Error("Expected remote to not be newer")
+	}
+}
+
+func TestLaunchpadReleaseInvalid(t *testing.T) {
+	remote := version.LaunchpadRelease{TagName: "invalid"}
+
+	if remote.IsNewer() {
+		t.Error("Expected remote to not be newer")
+	}
+}
