944af0674ef8de351867b6a14b6406e262150567
James Nesbitt <james.r.nesbitt@gmail.com>
2025-03-19 16:48:10 +0200

    530 host mcr upgrade skip (#558)
    * 530 MCR upgrade skip for host
    
    - new host configuration `MCRUpgradeSkip` boolean. If true then the MCR Upgrade phase will skip the host
    
    ALSO
    
    - update the release process after the repos were renamed
    
    Signed-off-by: James Nesbitt <jnesbitt@mirantis.com>

--- release.sh
+++ release.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+REPO_NAME="${REPO_NAME:-launchpad}"
+REPO_USER="${REPO_USER:-Mirantis}"
+
 if [ -z "${TAG_NAME}" ]; then
   echo "TAG_NAME not set"
   exit 1
@@ -11,7 +14,7 @@ artifacts=$(find ${artifact_path}/* -exec basename {} \;)
 echo "Releasing with:"
 for artifact in ${artifacts}; do echo "- ${artifact}"; done
 
-curl -L https://github.com/github-release/github-release/releases/download/v0.8.1/linux-amd64-github-release.bz2 > ./github-release.bz2
+curl -L https://github.com/github-release/github-release/releases/download/v0.10.0/linux-amd64-github-release.bz2 > ./github-release.bz2
 bunzip2 -f ./github-release.bz2
 chmod +x ./github-release
 
@@ -21,24 +24,24 @@ else
   releaseopt=""
 fi
 
-echo "Creating release named ${TAG_NAME} in MCC repo"
+echo "Creating release named ${TAG_NAME} in ${REPO_NAME} repo"
 
 ./github-release release \
   $releaseopt \
-  --user Mirantis \
-  --repo mcc \
+  --user "${REPO_USER}" \
+  --repo "${REPO_NAME}" \
   --tag "${TAG_NAME}" \
   --name "${TAG_NAME}"
 
 sleep 10
 
-echo "Uploading the artifacts to ${TAG_NAME} in MCC repo"
+echo "Uploading the artifacts to ${TAG_NAME} in ${REPO_NAME} repo"
 
 for artifact in ${artifacts}
 do
    ./github-release upload \
-    --user Mirantis \
-    --repo mcc \
+    --user "${REPO_USER}" \
+    --repo "${REPO_NAME}" \
     --tag "${TAG_NAME}" \
     --name "${artifact}" \
     --file "${artifact_path}/${artifact}"
