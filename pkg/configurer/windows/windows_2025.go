65567fd5d8008a4541b7593c6a9af7f379483d19
Jim Carroll <pcarroll@mirantis.com>
2025-04-09 06:27:58 +1200

    feat: add support for Windows Server 2025 (#568)
    - feat: add support for Windows Server 2025
    - style: typo in comment

--- pkg/configurer/windows/windows_2025.go
+++ pkg/configurer/windows/windows_2025.go
@@ -7,23 +7,23 @@ import (
 	"github.com/k0sproject/rig/os/registry"
 )
 
-// Windows2022Configurer is a Windows 2022 configurer implementation.
-type Windows2022Configurer struct {
+// Windows2025Configurer is a Windows 2025 configurer implementation.
+type Windows2025Configurer struct {
 	configurer.WindowsConfigurer
 }
 
 // InstallMKEBasePackages installs the needed base packages on Ubuntu.
-func (c Windows2022Configurer) InstallMKEBasePackages(_ os.Host) error {
+func (c Windows2025Configurer) InstallMKEBasePackages(_ os.Host) error {
 	return nil
 }
 
 func init() {
 	registry.RegisterOSModule(
 		func(os rig.OSVersion) bool {
-			return os.ID == "windows" && os.Version == "10.0.20348"
+			return os.ID == "windows" && os.Version == "10.0.26100"
 		},
 		func() interface{} {
-			return Windows2022Configurer{}
+			return Windows2025Configurer{}
 		},
 	)
 }
