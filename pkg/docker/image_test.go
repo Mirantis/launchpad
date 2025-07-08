b96fa80aed06f6b0acf65d35bf18b673a9dd36c9
James Nesbitt <jnesbitt@mirantis.com>
2025-07-08 12:09:34 +0300

    PRODENG-3066 docker images list regex refactor
    - refactor the regex for interpreting command output as a list of images
      the regex was overly permissive probably due to golang string simplifications
    - added a test for the MKE387 output which was the trigger for the change
    
    Signed-off-by: James Nesbitt <jnesbitt@mirantis.com>

--- pkg/docker/image_test.go
+++ pkg/docker/image_test.go
@@ -37,3 +37,24 @@ docker.io/foo/bar2:1.2.3`)
 	require.Equal(t, "custom.example.com/repo/bar:1.2.3", moved[0].String())
 	require.Equal(t, "custom.example.com/repo/bar2:1.2.3", moved[1].String())
 }
+
+// MKE3.8.7 forced us to refine the regex used. This test uses some real MKE output w/ --debug to confirm
+func TestMKE387OutputTest(t *testing.T) {
+	mke387output := `time="2025-07-03T01:00:12Z" level=debug msg="Verifying docker.sock"
+time="2025-07-03T01:00:12Z" level=debug msg="Checking for compatible kernel version"
+time="2025-07-03T01:00:12Z" level=debug msg="Kernel version 6.8.0-1030-aws is compatible"
+time="2025-07-03T01:00:12Z" level=info msg="Skipping compatible engine version check for --force-engine-minimum"
+time="2025-07-03T01:00:12Z" level=debug msg="Start finding bootstrap container"
+time="2025-07-03T01:00:12Z" level=debug msg="Found 1 container(s) running the bootstrap image"
+time="2025-07-03T01:00:12Z" level=debug msg="Container \"/priceless_edison\" running: /bin/ucp-tool images"
+time="2025-07-03T01:00:12Z" level=info msg="Bootsrapper image org: msr.ci.mirantis.com/mirantiseng"
+time="2025-07-03T01:00:12Z" level=info msg="Bootsrapper image version: 3.8.7"
+msr.ci.mirantis.com/mirantiseng/ucp-agent:3.8.7
+msr.ci.mirantis.com/mirantiseng/ucp-alertmanager:3.8.7
+msr.ci.mirantis.com/mirantiseng/ucp-auth-store:3.8.7`
+	images := AllFromString(mke387output)
+	require.Equal(t, 3, len(images))
+	require.Equal(t, "msr.ci.mirantis.com/mirantiseng/ucp-agent:3.8.7", images[0].String())
+	require.Equal(t, "msr.ci.mirantis.com/mirantiseng/ucp-alertmanager:3.8.7", images[1].String())
+	require.Equal(t, "msr.ci.mirantis.com/mirantiseng/ucp-auth-store:3.8.7", images[2].String())
+}
